2df002ab5c24132701abcbfdf85d4939bb5a28eb
Linr <www@xiaogezi.cn>
2017-01-18 21:32:45 +0800

    +微软自拍
    +微软自拍

--- app/linrwebapp.js
+++ app/linrwebapp.js
@@ -1,1 +1,1 @@
-{"services": [{"name": "baidu", "url": "http://www.baidu.com", "script": "alert(1);"}]}
+{"services": [{"name": "微软自拍", "url": "https://selfie.microsoftcafe.net/", "img": "http://9.pic.pc6.com/thumb/up/2016-8/201682093551182091_128_128.jpg"}]}
