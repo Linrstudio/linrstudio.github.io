faccf3eb971eba3e854bcffcc894268452bc0a46
Linr <www@xiaogezi.cn>
2017-02-15 12:42:42 +0800

    [!]alipay.com
    [!]alipay.com

---
+++ app/linrapp/alipay.com.js
@@ -0,0 +1,19 @@
+{
+        "button": [
+            {
+                "name": "生活缴费", 
+                "sub_button": [
+                    {
+                        "type": "view", 
+                        "name": "水电煤", 
+                        "url": "https://jiaofei.alipay.com/fare/ebppChargeEntering.htm", 
+                    }
+                ]
+            }, 
+            {
+                "name": "我的支付宝", 
+                "type": "view", 
+                "url": "https://my.alipay.com/"
+            }
+        ]
+    }
